793316519b971f448943ccd4cc25994c6ae3213d
Shreshth <shreshth28srivastava@gmail.com>
2020-11-21 14:38:20 +0530

    Added Note Entity Model Class

---
+++ app/src/main/java/com/shreshthsrivastava/happ/model/Note.java
@@ -0,0 +1,50 @@
+package com.shreshthsrivastava.happ.model;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "note_table")
+public class Note {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    private String title;
+
+    private String description;
+
+    private int rating;
+
+    private String date;
+
+    public Note(String title, String description, int rating, String date) {
+        this.title = title;
+        this.description = description;
+        this.rating = rating;
+        this.date = date;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public String getDate() {
+        return date;
+    }
+}
